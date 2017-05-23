5fd575d0e8bee507c5e809e850ba70c04ae0f620
zhoulizhong <919781714@qq.com>
2017-05-23 16:40:39 +0800

    repayplan

--- application/hmvc/admin/views/loan/oplog.php
+++ application/hmvc/admin/views/loan/oplog.php
@@ -12,16 +12,22 @@
     <link href="<?php echo RS_PATH?>admin/css/flexigrid.css?v=201705031531" rel="stylesheet" type="text/css" />
     <link href="<?php echo RS_PATH?>css/font-awesome.min.css" rel="stylesheet" type="text/css" />
     <link href="<?php echo RS_PATH?>jquery/perfect-scrollbar.min.css" rel="stylesheet" type="text/css" />
+    <link href="<?php echo RS_PATH?>jquery/jquery.daterangepicker.css" rel="stylesheet" type="text/css" />
     <script type="text/javascript" charset="utf-8" src="<?php echo RS_PATH?>admin/js/language_<?php echo strtolower(\Base::getConfig()->getLanguageTypeDirName());?>.js"></script>
     <script type="text/javascript" charset="utf-8" src="<?php echo RS_PATH?>jquery/jquery-1.8.3.min.js"></script>
     <script type="text/javascript" charset="utf-8" src="<?php echo RS_PATH?>jquery/jquery.nicescroll.js"></script>
     <script type="text/javascript" charset="utf-8" src="<?php echo RS_PATH?>jquery/perfect-scrollbar.min.js"></script>
     <script type="text/javascript" charset="utf-8" src="<?php echo RS_PATH?>artdialog/dialog-plus-min.js"></script>
+    <script type="text/javascript" charset="utf-8" src="<?php echo RS_PATH?>moment.min.js"></script>
+    <script type="text/javascript" charset="utf-8" src="<?php echo RS_PATH?>jquery/jquery.daterangepicker.js"></script>
     <script type="text/javascript" charset="utf-8" src="<?php echo RS_PATH?>admin/js/laymain.js"></script>
     <script type="text/javascript" charset="utf-8" src="<?php echo RS_PATH?>admin/js/common.js?v=201705041335"></script>
     <script type="text/javascript" charset="utf-8" src="<?php echo RS_PATH?>admin/js/flexigrid.js"></script>
     <!--[if lt IE 9]>
     <script type="text/javascript" charset="utf-8" src="<?php echo RS_PATH?>html5.js"></script>
+    <![endif]-->
+    <!--[if IE]>
+    <script type="text/javascript" charset="utf-8" src="<?php echo RS_PATH?>html5shiv.min.js"></script>
     <![endif]-->
 
 </head>
@@ -77,17 +83,17 @@
                 </dl>
 
                 <dl>
-                    <dt>开始日期</dt>
+                    <dt>按时间段查询</dt>
                     <dd>
-
+                          <span id="daterange">
+                            <input class="s-input-txt" type="text" readonly="true" value="" id="datestart" name="datestart">
+         	                至
+         	                <input class="s-input-txt" type="text" readonly="true" value="" id="dateend" name="dateend">
+                          </span>
                     </dd>
                 </dl>
-                <dl>
-                    <dt>结束日期</dt>
-                    <dd>
 
-                    </dd>
-                </dl>
+
             </div>
         </div>
         <div class="bottom"><a href="javascript:void(0);" id="submit" style="color: #ffffff;" class="btn btn-green mr5">提交查询</a><a href="javascript:void(0);" id="reset" style="color: #ffffff;" class="btn btn-orange" title="撤销查询结果，还原列表项所有内容"><i class="fa fa-retweet"></i>撤销</a></div>
@@ -101,7 +107,7 @@
                 {display: '贷款编号', name : 'deal_id', width : 50, sortable : false, align: 'center'},
                 {display: '借款用户', name : 'user_id', width : 100, sortable : false, align : 'center'},
                 {display: '操作阶段', name : 'op_name', width : 80, sortable : false, align: 'left'},
-                {display: '日志信息', name : 'log', width : 500, sortable : false, align: 'left'},
+                {display: '日志信息', name : 'log', width : 300, sortable : false, align: 'left'},
                 {display: '结果', name : 'op_result', width : 80, sortable : false, align: 'center'},
                 {display: '操作人', name : 'admin_id', width : 150, sortable : false, align: 'center'},
                 {display: '操作时间', name : 'create_time', width : 150, sortable : false, align: 'center'},
@@ -109,12 +115,6 @@
             ],
             searchitems : [
                 {display: '贷款编号', name : 'deal_id'},
-            ],
-            buttons : [
-                {display: '<i class="fa"></i> 今', name : 'today',  title : '今', onpress : flexPress },
-                {display: '<i class="fa "></i> 昨', name : 'yesterday', title : '昨', onpress : flexPress },
-                {display: '<i class="fa"></i> 近一周', name : 'week',  title : '近一周', onpress : flexPress },
-                {display: '<i class="fa "></i> 近一月', name : 'month', title : '近一月', onpress : flexPress }
             ],
             sortname: "create_time",
             sortorder: "desc",
@@ -134,21 +134,26 @@
         });
 
     });
-    function flexPress(name, grid) {
-        if(name=='today'){
-            $("#flexitable").flexOptions({url: '<?php echo adminUrl('loan_loan','all_op_log_json');?>&'+'time='+name}).flexReload();
+    $('#daterange').dateRangePicker({
+        shortcuts:
+        {
+            'prev-days': [1,3,5,7,30,60],
+            'prev' : null,
+        },
+        endDate:'<?php echo date('Y-m-d',time());?>',
+        getValue: function()
+        {
+            if ($('#datestart').val() && $('#dateend').val() )
+                return $('#datestart').val() + ' to ' + $('#dateend').val();
+            else
+                return '';
+        },
+        setValue: function(s,s1,s2)
+        {
+            $('#datestart').val(s1);
+            $('#dateend').val(s2);
         }
-        if(name=='yesterday'){
-            location.href='<?php echo adminUrl('loan_loan','all_op_log_json');?>';
-        }
-        if(name=='week'){
-            location.href='<?php echo adminUrl('loan_loan','all_op_log_json');?>';
-        }
-        if(name=='month'){
-            location.href='<?php echo adminUrl('loan_loan','all_op_log_json');?>';
-        }
-    }
-
+    });
     $('#syshelp').on("click",function(){
         var d = dialog({
             content: "<?php echo \Core::L('loan_all_help');?>",
@@ -156,8 +161,7 @@
         });
         d.show(this);
     });
-
-
+    $(".date-picker-wrapper").css('z-index',999);
 </script>
 </body>
 </html>
