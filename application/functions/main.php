d00ab6d0d9dfa6625d09488c780d64171ca372aa
luhuan <luhuan@xiaoshushidai.com>
2017-05-31 13:52:17 +0800

    增加getDateFromRange获取日期之间的全部日期数组

--- application/functions/main.php
+++ application/functions/main.php
@@ -367,4 +367,31 @@ function setTransactionId()
 
     return $transaction_id;
 }
+
+/**
+ * 获取指定日期段内每一天的日期
+ * @param  Date  $startdate 开始日期
+ * @param  Date  $enddate   结束日期
+ * @param  Blean $unixdate  是否是unix时间
+ * @return Array
+ */
+function getDateFromRange($startdate, $enddate,$unixdate=false){
+	if(!$unixdate){
+		$stimestamp = strtotime($startdate);
+    	$etimestamp = strtotime($enddate);
+	}else{
+		$stimestamp=$startdate;
+		$etimestamp=$enddate;
+	}
+    // 计算日期段内有多少天
+    $days = ($etimestamp-$stimestamp)/86400+1;
+
+    // 保存每天日期
+    $date = array();
+
+    for($i=0; $i<$days; $i++){
+        $date[] = date('Y-m-d', $stimestamp+(86400*$i));
+    }
+    return $date;
+}
 ?>
