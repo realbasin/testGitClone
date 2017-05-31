5501f39c366fc0800d2733eebe71e999b95fcef2
luhuan <luhuan@xiaoshushidai.com>
2017-05-31 18:27:55 +0800

    完成网站垫付统计

--- application/hmvc/admin/classes/controller/stat/platform.php
+++ application/hmvc/admin/classes/controller/stat/platform.php
@@ -185,7 +185,66 @@ class  controller_stat_platform extends controller_sysBase {
 	
 	//垫付统计
 	public function do_platformPayment(){
-		
+		$datestart = \Core::postGet('datestart');
+		$dateend = \Core::postGet('dateend');
+		if (!$datestart || !$dateend) {
+			$datestart = 0;
+			$dateend = 0;
+		}
+		\Core::view() -> set('datestart', $datestart);
+		\Core::view() -> set('dateend', $dateend);
+		\Core::view() -> load('stat_platformPayment');
+	}
+	
+	public function do_platformPayment_json(){
+		$datestart = \Core::postGet('datestart');
+		$dateend = \Core::postGet('dateend');
+		if (!$datestart || !$dateend) {
+			showJSON('100', '请选择日期范围');
+		}
+		$startStamp=strtotime($datestart);
+		$endStamp=strtotime($dateend);
+		if($startStamp>$endStamp){
+			showJSON('101', '开始日期不能大于结束日期');
+		}
+		if((($endStamp-$startStamp)/86400+1)>C('stat_date_range_max')){
+			showJSON('102', '日期范围不能大于'.C('stat_date_range_max').'天');
+		}
+
+		$daoPayment = \Core::dao('loan_generationrepay');
+		$datas = $daoPayment->getStatPlatformPayment($startStamp, $endStamp);
+		if(!$datas){
+			$row=array();
+			$row['createdate']=$datestart;
+			$row['paymenttotal']=0;
+			$row['feetotal']=0;
+			$row['imposetotal']=0;
+			$row['managetotal']=0;
+			$datas[]=$row;
+			$row['createdate']=$dateend;
+			$datas[]=$row;
+		}
+		showJSON('200', '', $datas);
+	}
+	
+	public function do_platformPayment_export(){
+		$datestart = \Core::get('datestart');
+		$dateend = \Core::get('dateend');
+		$datestart = $datestart ? $datestart : date('Y-m-d', strtotime('-30 day'));
+		$dateend = $dateend ? $dateend : date('Y-m-d', time());
+		//Excel头部
+		$header = array();
+		$header['日期'] = 'date';
+		$header['代还本息总额'] = 'price';
+		$header['代还管理费总额'] = 'price';
+		$header['代还罚息总额'] = 'price';
+		$header['代还逾期管理费总额'] = 'price';
+
+		$daoPayment = \Core::dao('loan_generationrepay');
+		$datas = $daoPayment->getStatPlatformPayment(strtotime($datestart), strtotime($dateend));
+		//导出
+		$this -> log('导出网站垫付统计(' . $datestart . ' - ' . $dateend . ')', 'export');
+		exportExcel('网站垫付统计(' . $datestart . ' - ' . $dateend . ')', $header, $datas);
 	}
 	
 	//审核汇总
