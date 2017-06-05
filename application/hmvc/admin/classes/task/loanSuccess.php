43d41cd4777cd466e9c5d1fefac0311a7e298985
zhoulizhong <919781714@qq.com>
2017-06-05 18:24:42 +0800

    放款，生成还款计划、回款计划调整

--- application/hmvc/admin/classes/task/loanSuccess.php
+++ application/hmvc/admin/classes/task/loanSuccess.php
@@ -7,9 +7,22 @@ class  task_loanSuccess extends Task {
 	 *@option deal_id int 借款ID
 	 */
 	public function execute(CliArgs $args) {
-		$deal_id=$args->get('deal_id');
-		if($deal_id && is_numeric($deal_id)){
-			$loanBaseDao->update(array('is_effect' => 1, 'loan_status' => 1), array('id' => $deal_id));
+		$user_id = $args->get('user_id');
+		echo $user_id;
+		if($user_id && is_numeric($user_id)){
+			$admin_id = \Core::dao('user_user')->getUser($user_id,'id,admin_id,platform_code');
+			if($admin_id[$user_id]) {
+				$loanBid = \Core::dao('loan_loanbid')->getOneLoanById($user_id,'load_money');
+				$loanBase = \Core::dao('loan_loanbase')->getloanbase($user_id,'id,name,repay_time_type,repay_time');
+				$admin_deal_info = array_merge($loanBase,$loanBid);
+				$adminstatus = \Core::business('sys_admin_admin')->adminReferrals($admin_id[$user_id],$admin_deal_info);
+				if($adminstatus['status'] == 1) {
+					$result['message'] = $adminstatus['message'];
+					$result['status'] = 1;
+				}
+			}
+
+			//$loanBaseDao->update(array('is_effect' => 1, 'loan_status' => 1), array('id' => $deal_id));
 			//发送短信发送邮件
 
 		    //是否存在优投用户，存在发送推送
