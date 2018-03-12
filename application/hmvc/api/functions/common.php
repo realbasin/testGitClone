78429da0570ce30c9e35da07474af8f001751e51
hmxingkong <hmxingkong@126.com>
2018-03-12 10:21:38 +0800

    添加首页成交金额格式化统计函数

--- application/hmvc/api/functions/common.php
+++ application/hmvc/api/functions/common.php
@@ -26,3 +26,42 @@ function getSiteConfig()
 
     return $site;
 }
+
+
+/*首页成交金额格式化统计*/
+function formatMoneyIndex($number,$decimal_count=0){
+    if($number == "")
+        return "0.00";
+
+    $attr_number = explode(".",$number);
+    if(strpos($attr_number[0],',') == false)
+        $num_str = $attr_number[0];
+    else
+        $num_str = $attr_number[0];
+
+    $yi = 100000000;
+    $wan = 10000;
+
+    if($num_str > $yi){
+        $yi_str = $num_str/$yi;
+        $yi_arr = explode('.',$yi_str);
+        $wan_str = $yi_arr[1]/$wan;
+        $wan_arr = explode('.',$wan_str);
+
+        $data_str = $yi_arr[0].'<i>亿</i>'.$wan_arr[0].'<i>万</i>';
+        return $data_str;
+    }elseif($num_str > $wan){
+        $wan_str = $num_str/$wan;
+        $wan_arr = explode('.',$wan_str);
+
+        if($decimal_count)
+            $data_str = $wan_arr[0].'.'.substr($wan_arr[1],0,$decimal_count).'<i>万</i>';
+        else
+            $data_str = $wan_arr[0].'<i>万</i>';
+        return $data_str;
+    }else{
+        return $num_str.'<i>元</i>';
+    }
+
+}
+
